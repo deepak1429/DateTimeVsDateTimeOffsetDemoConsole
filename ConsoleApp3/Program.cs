2a8d718879724e1b78301e1622e8a8059389a62c
Deepak Yadav <deeyadav2@publicisgroupe.net>
2020-01-13 11:47:36 +0530

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                DateTimeOffset localTime = DateTimeOffset.Now;
+                DateTimeOffset utcTime = DateTimeOffset.UtcNow;
+                DateTime dateTime = DateTime.Now;
+                DateTime dateTimeutc = DateTime.UtcNow;
+
+                Console.WriteLine("Local Time:          {0}", localTime.ToString("T"));
+                Console.WriteLine("Difference from UTC: {0}", localTime.Offset.ToString());
+                Console.WriteLine("UTC:                 {0}", utcTime.ToString("T"));
+                Console.WriteLine("datetime:                 {0}", dateTime.ToString("T"));
+                Console.WriteLine("datetime UTC:                 {0}", dateTimeutc.ToString("T"));
+                Console.ReadLine();
+            
+        }
+    }
+    
+}
